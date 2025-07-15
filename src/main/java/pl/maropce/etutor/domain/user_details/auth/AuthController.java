aa94e1be687a07eaf00bf118c0f55d41e259d6a9
maropce <radosny.one@gmail.com>
2025-07-15 22:04:04 +0200

    chore(code): clean up unused imports

--- src/main/java/pl/maropce/etutor/domain/user_details/auth/AuthController.java
+++ src/main/java/pl/maropce/etutor/domain/user_details/auth/AuthController.java
@@ -2,21 +2,19 @@ package pl.maropce.etutor.domain.user_details.auth;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.maropce.etutor.config.jwt.JwtUtil;
+import pl.maropce.etutor.domain.user.AppUser;
 import pl.maropce.etutor.domain.user.AppUserService;
 import pl.maropce.etutor.domain.user_details.AppUserDetails;
 
