8ba6bd311858c8d55332773f1f05aee331dad034
maropce <radosny.one@gmail.com>
2025-07-31 21:10:06 +0200

    refactor: added 3 unique users witch different roles each (ADMIN, TEACHER, STUDENT)

--- src/main/java/pl/maropce/etutor/config/AppTest.java
+++ src/main/java/pl/maropce/etutor/config/AppTest.java
@@ -39,57 +39,100 @@ public class AppTest {
     public ApplicationRunner initializeDatabase() {
         return args -> {
 
-            AppUser appUser = AppUser.builder()
-                    .firstName("Marek")
-                    .lastName("Krosny")
-                    .phoneNumber("654753734")
-                    .build();
-
-            AppUserDetails appUserDetails = AppUserDetails.builder()
-                    .appUser(appUser)
-                    .role(Role.TEACHER)
-                    .username("admin")
-                    .password(passwordEncoder.encode("admin"))
-                    .build();
-
-            appUser.setAppUserDetails(appUserDetails);
-
-            appUserDetailsRepository.save(appUserDetails);
-
-
-            Quiz quiz = Quiz.builder()
-                    .title("Quiz 1")
-                    .build();
-            quizRepository.save(quiz);
-
-            Question question1 = Question.builder()
-                    .question("Jaki język jest najlepszy?")
-                    .type(QuestionType.SINGLE)
-                    .options(List.of("Python", "Java", "C#", "C++"))
-                    .correctOptions(List.of("Java"))
-                    .quiz(quiz)
-                    .build();
-
-            Question question2 = Question.builder()
-                    .question("Które typy danych są poprawne w języku Java")
-                    .type(QuestionType.MULTI)
-                    .options(List.of("int", "float", "number", "string", "String"))
-                    .correctOptions(List.of("int", "float", "String"))
-                    .quiz(quiz)
-                    .build();
-
-            Question question3 = Question.builder()
-                    .question("Ułóż te typy danych w kolejności ich rozmiaru od najmniejszego do największego")
-                    .type(QuestionType.ORDER)
-                    .options(List.of("int", "short", "byte", "double"))
-                    .correctOptions(List.of("byte", "short", "int", "double"))
-                    .quiz(quiz)
-                    .build();
-
-            questionRepository.save(question1);
-            questionRepository.save(question2);
-            questionRepository.save(question3);
+            createUsers();
+            createQuizzes();
         };
     }
 
+    private void createUsers() {
+        AppUser appUser = AppUser.builder()
+                .firstName("Mirek")
+                .lastName("Adminowy")
+                .phoneNumber("654753734")
+                .build();
+
+        AppUserDetails appUserDetails = AppUserDetails.builder()
+                .appUser(appUser)
+                .role(Role.ADMIN)
+                .username("admin")
+                .password(passwordEncoder.encode("admin"))
+                .build();
+
+        appUser.setAppUserDetails(appUserDetails);
+
+        appUserDetailsRepository.save(appUserDetails);
+
+        /////////////////////////////////////////////////////////////
+
+        AppUser appUser2 = AppUser.builder()
+                .firstName("Juliusz")
+                .lastName("Ticzerowy")
+                .phoneNumber("834028457")
+                .build();
+
+        AppUserDetails appUserDetails2 = AppUserDetails.builder()
+                .appUser(appUser2)
+                .role(Role.TEACHER)
+                .username("teacher")
+                .password(passwordEncoder.encode("teacher"))
+                .build();
+
+        appUser2.setAppUserDetails(appUserDetails2);
+
+        appUserDetailsRepository.save(appUserDetails2);
+
+        /////////////////////////////////////////////////////////////
+
+        AppUser appUser3 = AppUser.builder()
+                .firstName("Mateusz")
+                .lastName("Uczniowy")
+                .phoneNumber("381203285")
+                .build();
+
+        AppUserDetails appUserDetails3 = AppUserDetails.builder()
+                .appUser(appUser3)
+                .role(Role.STUDENT)
+                .username("student")
+                .password(passwordEncoder.encode("student"))
+                .build();
+
+        appUser3.setAppUserDetails(appUserDetails3);
+
+        appUserDetailsRepository.save(appUserDetails3);
+    }
+
+    private void createQuizzes() {
+        Quiz quiz = Quiz.builder()
+                .title("Quiz 1")
+                .build();
+        quizRepository.save(quiz);
+
+        Question question1 = Question.builder()
+                .question("Jaki język jest najlepszy?")
+                .type(QuestionType.SINGLE)
+                .options(List.of("Python", "Java", "C#", "C++"))
+                .correctOptions(List.of("Java"))
+                .quiz(quiz)
+                .build();
+
+        Question question2 = Question.builder()
+                .question("Które typy danych są poprawne w języku Java")
+                .type(QuestionType.MULTI)
+                .options(List.of("int", "float", "number", "string", "String"))
+                .correctOptions(List.of("int", "float", "String"))
+                .quiz(quiz)
+                .build();
+
+        Question question3 = Question.builder()
+                .question("Ułóż te typy danych w kolejności ich rozmiaru od najmniejszego do największego")
+                .type(QuestionType.ORDER)
+                .options(List.of("int", "short", "byte", "double"))
+                .correctOptions(List.of("byte", "short", "int", "double"))
+                .quiz(quiz)
+                .build();
+
+        questionRepository.save(question1);
+        questionRepository.save(question2);
+        questionRepository.save(question3);
+    }
 }
