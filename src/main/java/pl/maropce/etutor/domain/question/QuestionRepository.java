93d0d256210b131a85c89f311114819bb2c07736
maropce <radosny.one@gmail.com>
2025-08-11 20:55:26 +0200

    refactor: remove deprecation from QuestinRepository

--- src/main/java/pl/maropce/etutor/domain/question/QuestionRepository.java
+++ src/main/java/pl/maropce/etutor/domain/question/QuestionRepository.java
@@ -3,7 +3,6 @@ package pl.maropce.etutor.domain.question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Deprecated
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, String> {
 }
