10d7e35c0d06611c23a2d2d2ecd0e7ed68d3e53a
maropce <radosny.one@gmail.com>
2025-08-20 02:18:01 +0200

    refactor(quiz): simplify quiz creation by saving quiz with questions in one call

--- src/main/java/pl/maropce/etutor/domain/quiz/QuizService.java
+++ src/main/java/pl/maropce/etutor/domain/quiz/QuizService.java
@@ -17,12 +17,10 @@ import java.util.List;
 public class QuizService {
 
     private final QuizRepository quizRepository;
-    private final QuestionRepository questionRepository;
     private final QuizMapper quizMapper;
 
-    public QuizService(QuizRepository quizRepository, QuestionRepository questionRepository, QuizMapper quizMapper) {
+    public QuizService(QuizRepository quizRepository, QuizMapper quizMapper) {
         this.quizRepository = quizRepository;
-        this.questionRepository = questionRepository;
         this.quizMapper = quizMapper;
     }
 
@@ -48,7 +46,6 @@ public class QuizService {
                .title(request.getTitle())
                .owner(appUserDetails.getAppUser())
                .build();
-       quizRepository.save(quiz);
 
        List<Question> questionList = new ArrayList<>();
        request.getQuestionList().forEach(question -> {
@@ -59,12 +56,12 @@ public class QuizService {
                    .correctOptions(question.getCorrectAnswer())
                    .quiz(quiz)
                    .build();
-           questionRepository.save(questionBuild);
            questionList.add(questionBuild);
        });
 
        quiz.setQuestionList(questionList);
 
-       return quizMapper.toDTO(quiz);
+        Quiz savedQuiz = quizRepository.save(quiz);
+        return quizMapper.toDTO(savedQuiz);
     }
 }
